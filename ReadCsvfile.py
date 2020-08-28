53cdac7221dfeb705760ed810514e7ff177d017c
prakashmahali <prakash.mahali@gmail.com>
2020-08-27 23:32:35 -0500

    Update ReadCsvfile.py

--- ReadCsvfile.py
+++ ReadCsvfile.py
@@ -3,6 +3,7 @@ from pyspark.context import SparkContext
 from awsglue.job import Job
 import sys
 from awsglue.utils import getResolvedOptions
+from awsglue.dynamicframe import DynamicFrame
 
 glueContext = GlueContext(SparkContext.getOrCreate())
 glueJob = Job(glueContext)
@@ -16,6 +17,15 @@ spark = glueContext.spark_session
 #df.show()
 
 #dfnew = spark.read.option("header","true").option("delimiter", ",").csv("s3a://pkm")
-df = spark.read.option("header","true").format("csv").load("s3a://pkm")
+df = spark.read.option("header","true").format("csv").load("s3a://pkm
+#inputGDF = glueContext.create_dynamic_frame_from_options(connection_type = "s3", connection_options = {"paths": ["s3://pkm"], "recurse":True}, format = "csv")
+#df=inputGDF.toDF()
 df.show(2)
+
+dynamic_dframe = DynamicFrame.fromDF(df, glueContext, "dynamic_df")
+ 
+##Write the DynamicFrame as a file in CSV format to a folder in an S3 bucket.
+##It is possible to write to any Amazon data store (SQL Server, Redshift, etc) by using any previously defined connections.
+retDatasink4 = glueContext.write_dynamic_frame.from_options(frame = dynamic_dframe, connection_type = "s3", connection_options = {"path": "s3://pkm-target"}, format = "csv", transformation_ctx = "datasink4")
+
 glueJob.commit()
