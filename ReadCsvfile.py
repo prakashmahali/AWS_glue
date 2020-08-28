71487f9e58bb80a2ab4d445385b78918ddd2fdb8
prakashmahali <prakash.mahali@gmail.com>
2020-08-27 23:12:12 -0500

    Update ReadCsvfile.py

--- ReadCsvfile.py
+++ ReadCsvfile.py
@@ -9,8 +9,13 @@ glueJob = Job(glueContext)
 args = getResolvedOptions(sys.argv,['JOB_NAME'])
 
 glueJob.init(args['JOB_NAME'],args)
-sparkSession = glueContext.sparkSession
+#sparkSession = glueContext.sparkSession
+spark = glueContext.spark_session
 
-df = sparkSession.read.csv("s3a://test-target")
-df.show()
+#df = sparkSession.read.csv("s3a://prakashmahali")
+#df.show()
+
+#dfnew = spark.read.option("header","true").option("delimiter", ",").csv("s3a://pkm")
+df = spark.read.option("header","true").format("csv").load("s3a://pkm")
+df.show(2)
 glueJob.commit()
